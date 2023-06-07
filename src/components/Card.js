8d36d898007cf35441110dc072fb25fe53ca338f
Adhitya Bagus Wicaksono <48616245+adhityabaguswicaksono@users.noreply.github.com>
2023-06-07 12:33:09 +0700

    Update Card.js
    Mengubah rounded-lg ke rounded-md

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,7 @@ export default function Card(props) {
       data-aos="fade-up"
     >
       {/* Card Start */}
-      <div className="group flex flex-row md:flex-col grow md:grow-0 md:w-80 md:h-full bg-sucor-500 p-4 md:px-8 md:py-12 rounded-lg gap-4 md:gap-4 text-white outline outline-1 outline-sucor-500 duration-300 hover:bg-sucor-700 hover:outline-sucor-700">
+      <div className="group flex flex-row md:flex-col grow md:grow-0 md:w-80 md:h-full bg-sucor-500 p-4 md:px-8 md:py-12 rounded-md gap-4 md:gap-4 text-white outline outline-1 outline-sucor-500 duration-300 hover:bg-sucor-700 hover:outline-sucor-700">
         <img
           src={element.img}
           className="rounded-full bg-white w-24 h-24 md:w-52 md:h-52 bg-cover self-center"
